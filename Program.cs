c6dcba208eafc9608b4150b590588eed65b2acad
Pryesh Gohil <45434154+pryeshgohil@users.noreply.github.com>
2023-03-26 13:53:06 +0100

    add ability to print red

--- Program.cs
+++ Program.cs
@@ -4,3 +4,6 @@ Console.ResetColor();
 Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine("--> I'm printing blue");
 Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("--> I'm printing red");
+Console.ResetColor();
