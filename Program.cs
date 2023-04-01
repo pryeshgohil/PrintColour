684f1040ced27fe0f87e95029b162f2e5e9a3aea
Pryesh Gohil <45434154+pryeshgohil@users.noreply.github.com>
2023-04-01 10:30:33 +0100

    add ability to print cyan

--- Program.cs
+++ Program.cs
@@ -7,3 +7,6 @@ Console.ResetColor();
 Console.ForegroundColor = ConsoleColor.Red;
 Console.WriteLine("--> I'm printing red");
 Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("--> I'm printing cyan");
+Console.ResetColor();
